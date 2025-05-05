4cba5f21977418e18b97fe3aae35b82e38b34191
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-05-05 17:24:47 -0300

    Update pistas.h

--- include/pistas.h
+++ include/pistas.h
@@ -1,1 +1,15 @@
+#ifndef PISTAS_H
+#define PISTAS_H
+
+typedef struct Pista {
+    char descricao[256];
+    int relevancia;
+    struct Pista* prox;
+} Pista;
+
+void adicionarPista(Pista** lista, const char* descricao, int relevancia);
+void mostrarPistas(Pista* lista);
+void ordenarPistas(Pista** lista);
+
+#endif
 
