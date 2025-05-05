4bf1c391aa61f76465413d6663c66143af4f32eb
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-05-05 17:24:20 -0300

    Update pistas.c

--- sources/pistas.c
+++ sources/pistas.c
@@ -1,1 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include "../include/pistas.h"
+
+void adicionarPista(Pista** lista, const char* descricao, int relevancia) {
+    Pista* nova = (Pista*)malloc(sizeof(Pista));
+    strcpy(nova->descricao, descricao);
+    nova->relevancia = relevancia;
+    nova->prox = *lista;
+    *lista = nova;
+}
 
